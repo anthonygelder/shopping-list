a231c00454de6f4b499a9d2269db5c45f4476f95
Anthony Gelder <anthony.gelder@gmail.com>
2019-07-13 11:56:02 -0700

    fully functioning

---
+++ index.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    // add item
+
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const formVal = $('#js-shopping-list-form').find('input[name="shopping-list-entry"]').val();
+    
+        $('.shopping-list').append('<li><span class="shopping-item">' + formVal + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    
+        $('#shopping-list-entry').val('');
+    });
+
+    // delete button
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+    });
+
+
+    // check button
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+});
